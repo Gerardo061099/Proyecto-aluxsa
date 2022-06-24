6662127a785a73259aa0dcfb986a3229c9bc6ee2
Gera <ger4rdojimenezc4zs@gmail.com>
2022-06-24 14:12:39 -0500

    se agrego categorias js y insertar_C php

--- inserta_c.php
+++ inserta_c.php
@@ -2,12 +2,12 @@
     include("abrir_conexion.php");
     $Descripcion = $_POST['descripcion'];
     $Material = $_POST['material'];
-    if ($Descripcion == "" || $Material == "") {
-        echo "Los datos recibidos estan vacios";
-    }else {
+    if ($Descripcion != "" || $Material != "") {
         mysqli_query($conexion,"INSERT INTO $tbcat_db3 (Descripcion,Material) values ('$Descripcion','$Material')");
-        echo "Los datos se insertaron correctamente";
-        include("cerrar_conexion.php");
+        echo "Proceso exitoso";
+    }else {
+        echo "Los datos recibidos estan vacios";
     }
+    include("cerrar_conexion.php");
 ?>
 
--- js/categorias.js
+++ js/categorias.js
@@ -2,30 +2,35 @@ function addcategorias(a) {
     a.preventDefault();
     var desc = document.getElementById('desc').value;
     var material = document.getElementById('material').value;
-    console.log("Descripcion: " + desc);
-    console.log("Material: " + material);
-    var datos = new FormData();
-    datos.append("descripcion", desc);
-    datos.append("material", material);
-    $.ajax({
-        type: "POST",
-        url: "inserta_c.php",
-        data: datos,
-        dataType: false,
-        success: function(mensaje) {
-            if (mensaje == "Los datos se insertaron correctamente") {
-                Swal({
+    if (desc != "" && material != "") {
+        console.log("Descripcion: " + desc);
+        console.log("Material: " + material);
+        var datos = new FormData();
+        datos.append("descripcion", desc);
+        datos.append("material", material);
+        $.ajax({
+            type: "POST",
+            url: "inserta_c.php",
+            data: datos,
+            processData: false,
+            contentType: false,
+            beforeSend: function() {
+                $('#cargando').html("Procesando la peticion...");
+            },
+            success: function(mensaje) {
+                alert(mensaje);
+                swal({
                     title: "El proceso finalizo",
                     text: "Los datos se insertaron correctamente",
                     icon: "success"
                 });
-            } else if (mensaje == "Los datos recibidos estan vacios") {
-                Swal({
-                    title: "Error",
-                    text: "Los datos recibidos están vacios",
-                    icon: "error"
-                });
-            }
-        }
-    });
+            },
+        });
+    } else {
+        swal({
+            title: "Error",
+            text: "Los datos recibidos están vacios",
+            icon: "error"
+        });
+    }
 }
